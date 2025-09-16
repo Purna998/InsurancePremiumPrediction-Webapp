34a8999c2d5eb3e0ad35687448c43592fbb60e4b
Purna Acharya <152753500+Purna998@users.noreply.github.com>
2025-09-16 21:27:13 +0545

    All update

--- insurance/settings.py
+++ insurance/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "insurance.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insuranceml',         # Replace with your PostgreSQL database name
+        'USER': 'postgres',         # Replace with your PostgreSQL username
+        'PASSWORD': 'root', # Replace with your PostgreSQL password
+        'HOST': 'localhost',            # Or your DB host
+        'PORT': '5432',   
     }
 }
 
